3bc0866dfa5b09f7eb8fb64da8ee3c22b2d10c94
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-06-11 21:35:31 -0500

    added a ;

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/bikes', (req, res) => {
   })
 
   app.get('/search', (req,res) => {
-    const {q} = req.query
+    const {q} = req.query;
     res.send(<h1>`Search Results for: ${q}`</h1>)
 })
 
